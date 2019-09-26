9ae1e86734fd6ee1e52f091a50220c77028109bc
Sandy Akbar <55833676+sandytwelve12@users.noreply.github.com>
2019-09-26 04:36:20 -0700

    Add files via upload

---
+++ PBO-ULANG-10114504-Latihan1-Biodata/src/pbo/ulang/pkg10114504/latihan1/biodata/PBOULANG10114504Latihan1Biodata.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10114504.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * NAMA     : Sandy Akbar
+ * KELAS    : PBO-Ulang
+ * NIM      : 10114504
+ * DESKRIPSI: Program ini berisi program Biodata diri
+ */
+public class PBOULANG10114504Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println ("NAMA\t\t: Sandy Akbar"); 
+        System.out.println ("ALAMAT\t\t: Jalan Hantap, Antapani"); 
+        System.out.println ("TEMPAT LAHIR\t: Bandung");
+        System.out.println ("TANGGAL LAHIR\t: 07 Desember 1995"); 
+        System.out.println ("---------------------");
+        System.out.println ("BOBI\t\t: Badminton, Sepak Bola & Lari"); 
+        System.out.println ("MAKES\t\t: Sate");
+        System.out.println ("MIKES\t\t: Salad Buah");
+}
+
+    }
+    
+}
